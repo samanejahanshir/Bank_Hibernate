ac0e566d3e1aaed75276fefe1956652d0f5f7103
unknown <jahanshir.sm92@gmail.com>
2021-12-12 00:26:29 -0800

    add method saveUserToDb in BankService

--- src/service/BankService.java
+++ src/service/BankService.java
@@ -1,4 +1,15 @@
 package service;
 
+import dao.UserDao;
+import models.User;
+
 public class BankService {
+    public  int saveUserToDb(User user){
+        UserDao userDao=new UserDao();
+        return userDao.save(user);
+    }
+    public int searchUser(User user,long cartNum){
+
+return  0;
+    }
 }
