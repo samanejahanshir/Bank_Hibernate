fd7218bcb42ce283d6c01170ce489fe0fa5a941a
unknown <jahanshir.sm92@gmail.com>
2021-12-12 00:25:21 -0800

    add methods withDraw and deposit and getBalance in UserService

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,17 @@
 package service;
 
 public class UserService {
+    public boolean withDraw(int amount){
+        return false;
+    }
+    public boolean deposit(int amount){
+        return false;
+
+    }
+    public double getBalance(){
+        return 0;
+
+    }
+
 
 }
