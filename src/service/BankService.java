22d9921cefd609b68e59aebb4442df2f7ae49368
unknown <jahanshir.sm92@gmail.com>
2021-12-12 05:55:26 -0800

    add method searchUser in BankService

--- src/service/BankService.java
+++ src/service/BankService.java
@@ -1,15 +1,23 @@
 package service;
 
+import dao.AccountDao;
 import dao.UserDao;
+import models.Account;
 import models.User;
 
+import java.sql.SQLException;
+
 public class BankService {
-    public  int saveUserToDb(User user){
+    public  int saveUserToDb(User user) throws SQLException, ClassNotFoundException {
         UserDao userDao=new UserDao();
         return userDao.save(user);
     }
-    public int searchUser(User user,long cartNum){
-
-return  0;
+    public  int saveAccountToDb(Account account){
+        AccountDao accountDao=new AccountDao();
+        return accountDao.save(account) ;
+    }
+    public User searchUser(User user,long cartNum) throws SQLException, ClassNotFoundException {
+        UserDao userDao=new UserDao();
+        return  userDao.getUser(user,cartNum);
     }
 }
