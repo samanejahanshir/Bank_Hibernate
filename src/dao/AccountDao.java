c5b02061d3c82adfff33e8468e81597ed6325593
unknown <jahanshir.sm92@gmail.com>
2021-12-11 22:27:22 -0800

    add service classes in package service

--- src/dao/AccountDao.java
+++ src/dao/AccountDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import models.Account;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class AccountDao {
+    public int save(Account account) {
+        Session session = AccessDao.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        int id = (int) session.save(account);
+        transaction.commit();
+        session.close();
+        return id;
+    }
 }
--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class UserDao {
+    public int save(User user) {
+        Session session = AccessDao.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        int id = (int) session.save(user);
+        transaction.commit();
+        session.close();
+        return id;
+    }
 }
